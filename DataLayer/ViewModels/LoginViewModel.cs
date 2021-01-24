ab4945f36178c7d17f48908b9ac2b7480bf97e13
Parsa79ar <parsa.ardebili@gmail.com>
2021-01-24 11:22:21 -0800

    first commit

---
+++ DataLayer/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200)]
+        public string UserName { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "مرا بخاطر بسپار")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ MyCms/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+using System.Web.Security;
+
+namespace MyCms.Controllers
+{
+    public class AccountController : Controller
+    {
+        private ILoginRepository loginRepository;
+        MyCmsContext db = new MyCmsContext();
+
+        public AccountController()
+        {
+            loginRepository = new LoginRepository(db);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel login, string ReturnUrl="/")
+        {
+            if (ModelState.IsValid)
+            {
+                if (loginRepository.IsExistUser(login.UserName, login.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(login.UserName, login.RememberMe);
+                    return Redirect(ReturnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError("UserName", "کاربری یافت نشد");
+                }
+            }
+            return View(login);
+        }
+
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/");
+        }
+
+    }
+}
